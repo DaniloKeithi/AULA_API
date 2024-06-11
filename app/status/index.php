3a8f474e9f880a5f91da2475a7840ad39e9222c2
Danilo Keithi Makiya Nakasone <100208589+DaniloKeithi@users.noreply.github.com>
2024-06-11 10:50:46 -0300

    Add files via upload

---
+++ app/status/index.php
@@ -0,0 +1,17 @@
+<?php
+     //AQUI VAI SER A NOSSA APLICAÇÃO
+     require_once __DIR__ . '/../../api/config.php';
+     require_once __DIR__ . '/../../api/response.php';
+
+     if(API_IS_ACTIVE){
+        echo Response::resposta(200, 'sucess', [
+            'versao_api' => API_VERSION,
+            'status' => 'ativa',
+        ]);
+     }else{
+        echo Response::resposta(200, 'sucess', [
+            'versao_api' => API_VERSION,
+            'status' => 'manutenção',
+        ]);
+     }
+?>
